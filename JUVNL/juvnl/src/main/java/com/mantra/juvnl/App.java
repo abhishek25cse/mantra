bca87dd76efa009afe8480fe44e31f2f02b71835
Abhishek <abhishek25cse@gmail.com>
2019-01-07 10:11:24 +0530

    initial commit for a spring boot project

--- JUVNL/juvnl/src/main/java/com/mantra/juvnl/App.java
+++ JUVNL/juvnl/src/main/java/com/mantra/juvnl/App.java
@@ -1,13 +1,40 @@
 package com.mantra.juvnl;
 
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends Application
 {
     public static void main( String[] args )
     {
-        System.out.println( " Hello World" );
+        launch(args);
     }
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		 primaryStage.setTitle("Mantra Solves");
+		 GridPane grid = new GridPane();
+		 grid.setAlignment(Pos.CENTER);
+		 grid.setHgap(10);
+		 grid.setVgap(10);
+		 grid.setPadding(new Insets(25, 25, 25, 25));
+		 
+		 Text name = new Text();
+		 
+
+		 Scene scene = new Scene(grid, 300, 275);
+		 
+		 primaryStage.setScene(scene);
+		 primaryStage.show();
+		
+	}
 }
