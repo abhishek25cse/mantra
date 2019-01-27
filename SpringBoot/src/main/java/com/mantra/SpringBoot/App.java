dbd749d084ad90b5cbf9e207230342fe6a4a708a
Abhishek <abhishek25cse@gmail.com>
2019-01-27 20:44:46 +0530

    api with xml and json response added

--- SpringBoot/src/main/java/com/mantra/SpringBoot/App.java
+++ SpringBoot/src/main/java/com/mantra/SpringBoot/App.java
@@ -4,11 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-/**
- * Hello world!
- *
- */
-
 @SpringBootApplication
 @ComponentScan("com.mantra")
 public class App 
--- SpringBoot/src/main/java/com/mantra/controller/PingController.java
+++ SpringBoot/src/main/java/com/mantra/controller/PingController.java
@@ -1,6 +1,8 @@
 package com.mantra.controller;
 
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +17,20 @@ public class PingController {
 		return "The service is up"; 
 	}
 	
-	@GetMapping("user")
+	@GetMapping(path = "user" , produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<UserTO> getUser() {
 		UserTO user =  new UserTO();		
 		user.setName("Abhishek");
-		user.setRole("dev");
+		user.setRole("dev-json");
+		ResponseEntity res = new ResponseEntity(user,HttpStatus.INTERNAL_SERVER_ERROR);
+		return res;
+	}
+	
+	@GetMapping(path = "userxml",  produces =  MediaType.APPLICATION_XML_VALUE)	
+	public ResponseEntity<UserTO> getUserXML() {
+		UserTO user =  new UserTO();		
+		user.setName("Abhishek");
+		user.setRole("dev-xml");
 		ResponseEntity res = new ResponseEntity(user,HttpStatus.INTERNAL_SERVER_ERROR);
 		return res;
 	}
