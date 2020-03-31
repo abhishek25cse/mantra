0579e2d22707784f6f1cc6683b5a770873bf4d7e
Abhishek <abhishek25cse@gmail.com>
2020-03-31 14:13:00 +0530

    the config add

--- SpringBoot/src/main/java/com/mantra/controller/ConfigAPIs.java
+++ SpringBoot/src/main/java/com/mantra/controller/ConfigAPIs.java
@@ -1,10 +1,25 @@
 package com.mantra.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mantra.domain.ConfigMap;
+import com.mantra.service.ConfigService;
+
 @RestController
+@RequestMapping(value="api/config")
 public class ConfigAPIs {
+	
+	@Autowired
+	ConfigService configService;
 	
 	@GetMapping(value="error") 
 	public String errorPage () {
@@ -14,6 +29,18 @@ public class ConfigAPIs {
 	@GetMapping(value="login") 
 	public String homePage () {
 		return "LOGIN done";
+	}
+	@GetMapping(value="all" , produces=MediaType.APPLICATION_JSON_UTF8_VALUE ) 
+	public ResponseEntity<List<ConfigMap>> fetchConfigMap() {
+		ResponseEntity<List<ConfigMap>> response = new ResponseEntity<List<ConfigMap>>(configService.fetchConfigMap(), HttpStatus.ACCEPTED);
+		return response;
+		
+	}
+	
+	@GetMapping(value="save" , produces=MediaType.APPLICATION_JSON_UTF8_VALUE ) 
+	public ResponseEntity<ConfigMap> fetchConfigMap(@RequestParam ConfigMap configMap) {
+		ResponseEntity<ConfigMap> response = new ResponseEntity<ConfigMap>(configService.saveConfigMap(configMap), HttpStatus.ACCEPTED);
+		return response;		
 	}
 
 
